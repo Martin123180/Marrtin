cc1d4cec12c84b89a976fc8fbe2c53b771d33100
Martin123180 <pengbin.fan@studytafensw.edu.au>
2025-10-29 16:39:10 +1100

    Add files via upload

---
+++ week 2.py
@@ -0,0 +1,23 @@
+# age = int(input('What is your age'))
+# print('Age:',age)
+# room = 111
+# print('The room number is:', room)
+# num1 = int(input('Enter your first number:'))
+# num2 = int(input('Enter your second number:'))
+# # print('The first number is;', num1)
+# # print('The first number is;', num1)
+# result = num1 + num2
+# print('The result is:', result)
+# hours = float(input("How many hours did you worked:"))
+# rate = float(input("What is the pay rate:"))
+# gross_pay = hours * rate
+# print("Your gross pay is:", gross_pay)
+# rate=0.85
+# formatted="{:.0%}".format(rate)
+# print(formatted)
+# amount = 500
+# payment = amount/12
+# print(format(payment, '.2f
+TAX_RATE = 0.07
+a=float(input())
+print(a*TAX_RATE).
